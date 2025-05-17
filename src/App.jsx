1d69d1725f343cbb84fd677ae7e575fdffb3635a
Youssef Ahmed <yousseffcai@gmail.com>
2025-05-17 04:12:18 +0300

    feat: enhance UI with loading indicators and improved search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import { useMovies, useSearchMovies } from "./modules/movies/movies.hooks";
 import ScrollDialog from "./components/ScrollDialog";
+import { Button, CircularProgress } from "@mui/material";
 
 function App() {
   const [query, setQuery] = useState("");
@@ -12,7 +13,13 @@ function App() {
 
   const displayedData = filteredData ?? defaultData;
 
-  if (isPending) return <div>Loading...</div>;
+  if (isPending)
+    return (
+      <CircularProgress
+        size="5rem"
+        className="absolute left-1/2 top-1/2 translate-[-50%]"
+      />
+    );
 
   function handleSearch() {
     setQuery(ref.current.value.trim());
@@ -24,31 +31,57 @@ function App() {
   }
 
   return (
-    <>
-      <input ref={ref} className="p-2 border" name="search"></input>
-      <button onClick={handleSearch}>Search</button>
-      <div className="flex flex-wrap gap-4 justify-between">
-        {displayedData.results.map((movie) => (
-          <div onClick={() => handleClick(movie.id)} className="border p-5">
-            <h1>{movie.title}</h1>
-            <img
-              className="mx-auto"
-              src={
-                "https://image.tmdb.org/t/p/w220_and_h330_face" +
-                movie.poster_path
-              }
-              loading="lazy"
-            />
-            <p>{movie.release_date}</p>
-          </div>
-        ))}
-        <ScrollDialog
-          openDialog={openDialog}
-          setOpenDialog={setOpenDialog}
-          id={movieId}
+    <main className="flex flex-col gap-5">
+      <h1 className="text-center text-5xl mt-10">Mini Movie Explorer</h1>
+      <div className="flex gap-2 ml-2">
+        <input
+          ref={ref}
+          className="p-2 border border-gray-400 rounded"
+          name="search"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
         />
+        <Button variant="contained" color="info" onClick={handleSearch}>
+          Search
+        </Button>
       </div>
-    </>
+      <div className="flex flex-wrap gap-5 justify-center">
+        {displayedData.results.map((movie) =>
+          movie.poster_path ? (
+            <div
+              onClick={() => handleClick(movie.id)}
+              className="border cursor-pointer rounded-lg grow text-center flex flex-col gap-4 py-4 max-w-[250px]"
+            >
+              <h1 className="text-xl font-semibold truncate mx-5">
+                {movie.title}
+              </h1>
+              <img
+                className="mx-auto"
+                src={
+                  "https://image.tmdb.org/t/p/w220_and_h330_face" +
+                  movie.poster_path
+                }
+                loading="lazy"
+              />
+              <p>
+                <b>Release Date: </b>
+                {movie.release_date}
+              </p>
+            </div>
+          ) : null
+        )}
+        {openDialog && (
+          <ScrollDialog
+            openDialog={openDialog}
+            setOpenDialog={setOpenDialog}
+            id={movieId}
+          />
+        )}
+      </div>
+    </main>
   );
 }
 
--- src/components/ScrollDialog.jsx
+++ src/components/ScrollDialog.jsx
@@ -1,13 +1,11 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import { useMovie } from "../modules/movies/movies.hooks";
 import CloseIcon from "@mui/icons-material/Close";
-import { Rating, Typography } from "@mui/material";
+import { CircularProgress, Rating } from "@mui/material";
 
 export default function ScrollDialog({ id, openDialog, setOpenDialog }) {
   const { data, isPending, error } = useMovie(id);
@@ -26,7 +24,6 @@ export default function ScrollDialog({ id, openDialog, setOpenDialog }) {
     }
   }, [openDialog]);
 
-  if (isPending) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
   return (
@@ -40,63 +37,74 @@ export default function ScrollDialog({ id, openDialog, setOpenDialog }) {
         PaperProps={{
           style: {
             maxWidth: "70%",
-            backgroundColor: "#121212",
+            backgroundColor: "#1c1c1c",
             color: "azure",
           },
         }}
       >
-        <div className="flex justify-between items-center mt-4">
-          <p className="text-2xl font-bold ml-6">{data.title}</p>
-          <DialogActions>
-            <CloseIcon
-              onClick={handleClose}
-              className="cursor-pointer mx-4"
+        {isPending ? (
+          <DialogContent dividers={true} className="w-[70vw] h-[50vh]">
+            <CircularProgress
+              size="5rem"
+              className="absolute left-1/2 top-1/2 translate-[-50%]"
             />
-          </DialogActions>
-        </div>
-        <DialogContent dividers={true}>
-          <DialogContentText
-            id="scroll-dialog-description"
-            ref={descriptionElementRef}
-            tabIndex={-1}
-            className="flex gap-10 justify-between"
-          >
-            <img
-              className="mx-auto"
-              src={
-                "https://image.tmdb.org/t/p/w220_and_h330_face" +
-                data.poster_path
-              }
-              loading="lazy"
-            />
-            <section className="text-gray-300 text-lg flex flex-col gap-4">
-              <div>
-                <p className="font-semibold">Description:</p>
-                <p>{data.overview}</p>
-              </div>
-              <div className="flex gap-2">
-                <p className="font-semibold">Release Date:</p>
-                <p>{data.release_date}</p>
-              </div>
-              <div className="flex gap-2">
-                <p className="font-semibold">Rating:</p>
-                <Rating
-                  name="read-only"
-                  size="large"
-                  value={data.vote_average}
-                  max={10}
-                  precision={0.1}
-                  readOnly
-                  sx={{
-                    "& .MuiRating-iconEmpty": {
-                      color: "#d1d5dc",
-                    },
-                  }}
+          </DialogContent>
+        ) : (
+          <>
+            <div className="flex justify-between items-center mt-4">
+              <p className="text-2xl font-bold ml-6">{data.title}</p>
+              <DialogActions>
+                <CloseIcon
+                  onClick={handleClose}
+                  className="cursor-pointer mx-4"
                 />
-              </div>
-            </section>
-          </DialogContentText>
-        </DialogContent>
+              </DialogActions>
+            </div>
+            <DialogContent dividers={true}>
+              <DialogContentText
+                id="scroll-dialog-description"
+                ref={descriptionElementRef}
+                tabIndex={-1}
+                className="flex gap-10 justify-between"
+              >
+                <img
+                  className="mx-auto"
+                  src={
+                    "https://image.tmdb.org/t/p/w220_and_h330_face" +
+                    data.poster_path
+                  }
+                  loading="lazy"
+                />
+                <section className="text-gray-300 text-lg flex flex-col gap-4">
+                  <div>
+                    <p className="font-semibold text-cyan-100">Description:</p>
+                    <p>{data.overview}</p>
+                  </div>
+                  <div className="flex gap-2">
+                    <p className="font-semibold text-cyan-100">Release Date:</p>
+                    <p>{data.release_date}</p>
+                  </div>
+                  <div className="flex gap-2">
+                    <p className="font-semibold text-cyan-100">Rating:</p>
+                    <Rating
+                      name="read-only"
+                      size="large"
+                      value={data.vote_average}
+                      max={10}
+                      precision={0.1}
+                      readOnly
+                      sx={{
+                        "& .MuiRating-iconEmpty": {
+                          color: "#d1d5dc",
+                        },
+                      }}
+                    />
+                  </div>
+                </section>
+              </DialogContentText>
+            </DialogContent>
+          </>
+        )}
       </Dialog>
     </React.Fragment>
   );
